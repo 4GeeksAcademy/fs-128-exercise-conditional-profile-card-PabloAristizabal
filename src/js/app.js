77727cb59373a235d1e08d07f2dfe28c729b4825
Pablo Aristizabal <pablo.aristizabal.p@gmail.com>
2025-11-28 11:34:52 +0000

    Modified

--- src/js/app.js
+++ src/js/app.js
@@ -33,10 +33,10 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1> ${variables.name || "Name"} ${variables.lastName ||
+          <h1> ${variables.name ? variables.name : "Name"} ${variables.lastName ||
     "LastName"} </h1>
-          <h2> ${variables.role || "Role"}  </h2>
-          <h3>${variables.city || "City"}, ${variables.country ||
+          <h2> ${variables.role ? variables.role : "Role"}  </h2>
+          <h3>${variables.city ? variables.city : "City"}, ${variables.country ||
     "Country"}</h3>
           <ul class="${variables.socialMediaPosition}">
             <li><a href=" ${
